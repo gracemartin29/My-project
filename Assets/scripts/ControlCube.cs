67746826b6e1391bb5af18d81562c8e3023c126d
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2025-01-21 14:01:52 +0000

    moving cube wasd

--- Assets/scripts/ControlCube.cs
+++ Assets/scripts/ControlCube.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class ControlCube : MonoBehaviour
 {
+    float speed = 0.002f;
+    float steer = 0.03f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +14,19 @@ public class ControlCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * 0.003f;
-
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A)){
+            transform.Rotate(0f, -steer, 0f, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(0f, steer, 0f, Space.Self);
+        }
     }
 }
